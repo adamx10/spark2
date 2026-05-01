ef73ca0570df8a5b1a5b95ca1650a3a0c5cafbf2
adam <hazimaa1254@gmail.com>
2026-05-01 17:05:16 +0100

    up to date

---
+++ spark2/src/components/header.tsx
@@ -0,0 +1,17 @@
+export const Header = () => 
+ {
+
+    return <>
+    <div className="flex justify-between  w-11/12 mx-auto  ">
+
+        
+        <h1 className="flex justify-start w-3xl mx-auto">spark</h1>
+
+        <ul className="flex gap-4  justify-end items-center mx-auto">
+            <li>days</li>
+            <li>history</li>
+            
+        </ul>
+    </div>
+    </>
+ }
